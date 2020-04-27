f8a3746803fca61766162c708c7de415b9e34574
Антон <qant1998@gmail.com>
2020-04-27 13:44:45 +0300

    update

--- src/main/resources/static/js/graph.js
+++ src/main/resources/static/js/graph.js
@@ -7,13 +7,13 @@ var chart = new Chart(ctx, {
     data: {
         labels: ['10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00'],
         datasets: [{
-            label: 'Графік',
+            label: 'Sensor1',
             backgroundColor: 'rgb(0, 0, 100)',
             borderColor: 'rgb(0, 0, 0)',
             fill: false,
             data: [16.5, 18, 12, 20, 24, 15, 18, 10],
         },{
-            label: 'График 2',
+            label: 'Sensor2',
             backgroundColor: 'rgb(100, 300, 100)',
             borderColor: 'rgb(0, 0, 0)',
             fill: false,
